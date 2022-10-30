91862a32c88cb2c748eea5155c6a3ecc562150cf
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-30 23:21:29 +0300

    add: jpa query using jpql  and map them to optional functions

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/CustomUser/CustomUserRepository.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/CustomUser/CustomUserRepository.java
@@ -1,9 +1,16 @@
 package com.slinger.farmerMerchantAuctionSystem.CustomUser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomUserRepository extends JpaRepository<CustomUser,Integer> {
-
+//    ?1 - positional query parameter
+    @Query("select u from CustomUser u where u.customUserEmail=?1")
+    Optional<CustomUser> findByEmail(String email);
+    @Query("select u from CustomUser u where u.customUserName=?1")
+    Optional<CustomUser> findByName(String name);
 }
