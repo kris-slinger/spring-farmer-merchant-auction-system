14af830fb642b3e7b3a718e2202e0ed06f60c7a6
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-31 16:03:02 +0300

    update file

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/FarmerMerchantAuctionSystemApplication.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/FarmerMerchantAuctionSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FarmerMerchantAuctionSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FarmerMerchantAuctionSystemApplication.class, args);
 	}
