09cb2f6dccf72a60c73e76f60f90868b991c1aab
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-31 16:00:59 +0300

    add: updateUserById view function and responseEntities

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/CustomUser/CustomUserController.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/CustomUser/CustomUserController.java
@@ -1,6 +1,8 @@
 package com.slinger.farmerMerchantAuctionSystem.CustomUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,21 +18,35 @@ public class CustomUserController {
         this.customUserService = customUserService;
     }
     @GetMapping
-    public List<CustomUser> getCustomUsers(){
-        return customUserService.getAllUsers();
+    public ResponseEntity<List<CustomUser>> getCustomUsers(){
+        List<CustomUser> users=customUserService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
     }
     @GetMapping(path = "{userId}")
     public Optional<CustomUser> getUserById(@PathVariable Integer userId){
         return customUserService.getUserById(userId);
     }
     @PostMapping(path="/new")
-    public void createNewCustomUser(
+    public ResponseEntity<CustomUser> createNewCustomUser(
             @Valid @RequestBody CustomUser customUser
     ){
-        customUserService.createUser(customUser);
+            System.out.println(customUser);
+            CustomUser user=customUserService.createUser(customUser);
+            return new ResponseEntity<CustomUser>(user,HttpStatus.OK);
     }
     @PutMapping(path="{userId}")
-    public void updateUserById( @Valid @RequestBody CustomUser user,@PathVariable Integer userId){
-        customUserService.updateUser(userId,user);
+    public ResponseEntity<CustomUser> updateUserById(@Valid @RequestBody CustomUser user, @PathVariable Integer userId){
+        try{
+            CustomUser updatedUser=customUserService.updateUser(userId,user);
+            return new ResponseEntity<CustomUser>(updatedUser,HttpStatus.ACCEPTED);
+        }
+        catch (IllegalStateException e){
+            return new ResponseEntity<CustomUser>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @DeleteMapping(path="{userId}")
+    public ResponseEntity<CustomUser> deleteUSerById(@PathVariable Integer userId){
+        customUserService.deleteUserById(userId);
+        return new ResponseEntity<CustomUser>(HttpStatus.OK);
     }
 }
