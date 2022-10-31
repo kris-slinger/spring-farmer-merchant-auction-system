26d86d0fc1cd01a13cbdbba7cf741cb0687bb338
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-31 16:06:05 +0300

    add: farmer repository

---
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Farmer/FarmerRepository.java
@@ -0,0 +1,6 @@
+package com.slinger.farmerMerchantAuctionSystem.Farmer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmerRepository extends JpaRepository<Farmer,Integer> {
+}
