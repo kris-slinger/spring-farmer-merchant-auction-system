d48e17a04e211cc8914833236fdf1922870dd2f1
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-01 10:13:40 +0300

    add: Product controller class

---
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Product/ProductController.java
@@ -0,0 +1,8 @@
+package com.slinger.farmerMerchantAuctionSystem.Product;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="")
+public class ProductController {
+}
