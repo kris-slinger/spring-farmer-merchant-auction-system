23c4512d7e75dacbcb08ced9d31738d541255cac
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-01 10:10:51 +0300

    add: repository anotation

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/Merchant/MerchantRepository.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Merchant/MerchantRepository.java
@@ -4,9 +4,10 @@ import com.slinger.farmerMerchantAuctionSystem.CustomUser.CustomUser;
 import com.slinger.farmerMerchantAuctionSystem.Farmer.Farmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Integer> {
     @Query("select u from Merchant u where u.merchantCustomUserId=?1")
     Optional<Merchant> findByCustomUserId(CustomUser customUserId);
