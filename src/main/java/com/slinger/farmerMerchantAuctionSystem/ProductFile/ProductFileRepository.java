f1fce0a8bb5f35b4fb74a37e21c222d325c2d48e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-01 10:16:35 +0300

    add: product file repo

---
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/ProductFile/ProductFileRepository.java
@@ -0,0 +1,6 @@
+package com.slinger.farmerMerchantAuctionSystem.ProductFile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFileRepository extends JpaRepository<ProductFile,Integer> {
+}
