34b1a52d11cf601be6f71e280d9c59de1ee58faf
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-01 10:10:26 +0300

    add: repository anotation

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/Farmer/FarmerRepository.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Farmer/FarmerRepository.java
@@ -1,6 +1,8 @@
 package com.slinger.farmerMerchantAuctionSystem.Farmer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FarmerRepository extends JpaRepository<Farmer,Integer> {
 }
