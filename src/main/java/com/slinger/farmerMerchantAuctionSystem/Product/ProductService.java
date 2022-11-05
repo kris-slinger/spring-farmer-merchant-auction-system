840291ec6df7c6602dc91cf663f3598428cdc0bd
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 04:48:48 +0300

    add: create and update product services

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/Product/ProductService.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Product/ProductService.java
@@ -2,6 +2,9 @@ package com.slinger.farmerMerchantAuctionSystem.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
     ProductRepository productRepository;
@@ -9,6 +12,40 @@ public class ProductService {
     public ProductService(ProductRepository productRepository){
         this.productRepository=productRepository;
     }
-
+    public Product createNewProduct(Product productDetail){
+        return productRepository.save(productDetail);
+    }
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+    public Product getProductById(Integer productId){
+       return productRepository.findById(productId).orElseThrow(
+               () ->{throw new IllegalStateException((
+                       "product with id"+productId+" not found"
+                       )
+               );
+               }
+        );
+    }
+    public Product updateProductById(Integer productId,Product productDetail){
+        Product product=productRepository.findById(productId).orElseThrow(
+               () ->{throw new IllegalStateException((
+                       "product with id"+productId+" not found"
+                       )
+               );
+               }
+        );
+        product.setProductFarmer(productDetail.getProductFarmer());
+        product.setProductCategory(productDetail.getProductCategory());
+        product.setProductName(productDetail.getProductName());
+        product.setProductLocation(productDetail.getProductLocation());
+        product.setProductDescription(productDetail.getProductDescription());
+        product.setProductPrice((productDetail.getProductPrice()));
+        product.setProductFile(productDetail.getProductFile());
+        return product;
+    }
+    public void deleteProduct(Integer productId){
+        productRepository.deleteById(productId);
+    }
 
 }
