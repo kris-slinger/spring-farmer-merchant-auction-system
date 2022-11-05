ce80ef2d7e91681ae78359876fe52e353e92c4c4
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 04:46:52 +0300

    add: create,update and delete services

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/ProductFile/ProductFileService.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/ProductFile/ProductFileService.java
@@ -12,10 +12,31 @@ public class ProductFileService {
     public ProductFileService(ProductFileRepository productFileRepository){
         this.productFileRepository=productFileRepository;
     }
+    public ProductFile createProductFile(ProductFile productFileDetail){
+        return productFileRepository.save(productFileDetail);
+    }
     public List<ProductFile> getAllProductFiles(){
         return productFileRepository.findAll();
     }
     public Optional<ProductFile> getProductFileById(Integer productFileId){
         return productFileRepository.findById(productFileId);
+    }
+    public ProductFile updateProductFileById(ProductFile productFileDetail, Integer productFileId){
+        ProductFile productFile=productFileRepository.findById(productFileId).orElseThrow(
+                () -> {
+                    throw new IllegalStateException("productfile with id "+productFileId+" does not exist");
+                });
+        productFile.setProductFileName(productFileDetail.getProductFileName());
+        productFile.setProductFileType(productFileDetail.getProductFileType());
+        productFile.setProductFileFile(productFileDetail.getProductFileFile());
+        return productFile;
+
+    }
+    public void deleteProductFileById(Integer productFileId){
+         ProductFile productFile=productFileRepository.findById(productFileId).orElseThrow(
+                () -> {
+                    throw new IllegalStateException("product file with id "+productFileId+" does not exist");
+                });
+         productFileRepository.deleteById(productFileId);
     }
 }
