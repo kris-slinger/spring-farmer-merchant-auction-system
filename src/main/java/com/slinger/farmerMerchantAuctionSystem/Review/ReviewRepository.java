f5e3889aba5f958c5f32dbc21a4cbc342372766b
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 04:49:15 +0300

    add: repo

--- src/main/java/com/slinger/farmerMerchantAuctionSystem/Review/ReviewRepository.java
+++ src/main/java/com/slinger/farmerMerchantAuctionSystem/Review/ReviewRepository.java
@@ -1,4 +1,8 @@
 package com.slinger.farmerMerchantAuctionSystem.Review;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository  extends JpaRepository<Review,Integer> {
 }
